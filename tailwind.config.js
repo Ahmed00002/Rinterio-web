5ac90a7d5cae898514bbb5686a63635be82f7a5e
Layek Ahmed Numan <reduanahmed773@gmail.com>
2024-08-23 16:05:21 -0500

    addtional section position top reduced

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,9 @@ module.exports = {
         "2xl": "1536px",
         // => @media (min-width: 1536px) { ... }
       },
+      daisyui: {
+        themes: false,
+      },
     },
   },
   plugins: [require("daisyui")],
